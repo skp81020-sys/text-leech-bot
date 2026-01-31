ca344f18e0d20b9807d2d5717240f30a64c83a98
skp81020-sys <skp81020@gmail.com>
2026-02-01 00:15:23 +0530

    this is my first commit

--- modules/vars.py
+++ modules/vars.py
@@ -5,4 +5,4 @@ API_HASH  = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 WEBHOOK = True  # Don't change this
-PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+PORT = int(os.environ.get("PORT", 6969))  # Default to 8000 if not set
